343871a26b2be71bb9738bd5e4957db2d8c27fdf
Chase Grimm <cmgrimm@iastate.edu>
2017-12-11 13:58:18 -0600

    begin websocket work

--- websocket.R
+++ websocket.R
@@ -2,7 +2,17 @@
 
 require(curl)
 
-con <- curl('https://api.bitfinex.com/v2/ticker/tBTCUSD')
+#ticker
+t_con <- curl('https://api.bitfinex.com/v2/ticker/tLTCUSD')
 
+#book
+b_con <- curl('https://api.bitfinex.com/v2/book/tLTCUSD/P0')
 
+pool <- new_pool()
 
+cb <- function(req){cat("done:", req$url, ": HTTP:", req$status, "\n")}
+
+curl_fetch_multi('https://api.bitfinex.com/v2/ticker/tLTCUSD', done = cb, pool = pool)
+curl_fetch_multi('https://api.bitfinex.com/v2/book/tLTCUSD/P0', done = cb, pool = pool)
+
+out <- multi_run(pool = pool) 
